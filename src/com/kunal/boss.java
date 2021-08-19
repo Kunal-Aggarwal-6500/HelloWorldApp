cd66759a0a131391930ac92ffb96203bdb14014b
kunal Aggarwal <84708473+Kunal-Aggarwal-6500@users.noreply.github.com>
2021-08-19 22:54:02 +0530

    Delete boss.java

--- src/com/kunal/boss.java
+++
@@ -1,9 +0,0 @@
-package com.kunal;
-
-public class boss {
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World!!");
-        System.out.println("My name is kunal ");
-    }
-}
